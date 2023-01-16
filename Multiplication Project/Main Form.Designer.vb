595e431d5561596c9600c0df8e6f7da80b69ae7e
Cody Moyer <Cody Moyer@LAPTOP-3K39O47A>
2023-01-16 14:22:43 -0500

    Add project files.

---
+++ Multiplication Project/Main Form.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblNameTitle = New System.Windows.Forms.Label()
+        Me.txtNumber = New System.Windows.Forms.TextBox()
+        Me.lblMultiName = New System.Windows.Forms.Label()
+        Me.btnForNext = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnDoLoop = New System.Windows.Forms.Button()
+        Me.lbMain = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'lblNameTitle
+        '
+        Me.lblNameTitle.AutoSize = True
+        Me.lblNameTitle.Location = New System.Drawing.Point(40, 20)
+        Me.lblNameTitle.Name = "lblNameTitle"
+        Me.lblNameTitle.Size = New System.Drawing.Size(77, 23)
+        Me.lblNameTitle.TabIndex = 0
+        Me.lblNameTitle.Text = "&Number:"
+        '
+        'txtNumber
+        '
+        Me.txtNumber.Location = New System.Drawing.Point(104, 20)
+        Me.txtNumber.Name = "txtNumber"
+        Me.txtNumber.Size = New System.Drawing.Size(37, 29)
+        Me.txtNumber.TabIndex = 1
+        '
+        'lblMultiName
+        '
+        Me.lblMultiName.AutoSize = True
+        Me.lblMultiName.Location = New System.Drawing.Point(43, 63)
+        Me.lblMultiName.Name = "lblMultiName"
+        Me.lblMultiName.Size = New System.Drawing.Size(161, 23)
+        Me.lblMultiName.TabIndex = 5
+        Me.lblMultiName.Text = "Multiplication table:"
+        '
+        'btnForNext
+        '
+        Me.btnForNext.Location = New System.Drawing.Point(173, 20)
+        Me.btnForNext.Name = "btnForNext"
+        Me.btnForNext.Size = New System.Drawing.Size(97, 28)
+        Me.btnForNext.TabIndex = 2
+        Me.btnForNext.Text = "&For...Next"
+        Me.btnForNext.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(173, 86)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(97, 28)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnDoLoop
+        '
+        Me.btnDoLoop.Location = New System.Drawing.Point(173, 53)
+        Me.btnDoLoop.Name = "btnDoLoop"
+        Me.btnDoLoop.Size = New System.Drawing.Size(97, 28)
+        Me.btnDoLoop.TabIndex = 3
+        Me.btnDoLoop.Text = "&Do...Loop"
+        Me.btnDoLoop.UseVisualStyleBackColor = True
+        '
+        'lbMain
+        '
+        Me.lbMain.FormattingEnabled = True
+        Me.lbMain.ItemHeight = 21
+        Me.lbMain.Location = New System.Drawing.Point(44, 89)
+        Me.lbMain.Name = "lbMain"
+        Me.lbMain.Size = New System.Drawing.Size(123, 151)
+        Me.lbMain.TabIndex = 7
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 21.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(311, 275)
+        Me.Controls.Add(Me.lbMain)
+        Me.Controls.Add(Me.btnDoLoop)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnForNext)
+        Me.Controls.Add(Me.lblMultiName)
+        Me.Controls.Add(Me.txtNumber)
+        Me.Controls.Add(Me.lblNameTitle)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.MaximizeBox = False
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Multiplication Table"
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+    Friend WithEvents lblNameTitle As System.Windows.Forms.Label
+    Friend WithEvents txtNumber As System.Windows.Forms.TextBox
+    Friend WithEvents lblMultiName As System.Windows.Forms.Label
+    Friend WithEvents btnForNext As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents btnDoLoop As Button
+    Friend WithEvents lbMain As ListBox
+End Class
---
+++ Multiplication Project/Main Form.vb
@@ -0,0 +1,67 @@
+﻿' Name:         Multiplication Project
+' Purpose:      Displays a multiplication table that shows the multiplicand, multiplier, and product.
+' Programmer:   <your name> on <current date>
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Public Class frmMain
+
+    Private Sub btnDoLoop_Click(sender As Object, e As EventArgs) Handles btnDoLoop.Click
+        ' Displays a multiplication table.
+        Dim txtNumber2 As Integer = 1
+        lbMain.Items.Clear()
+        lbMain.Refresh()
+
+        Do While (txtNumber2 <= 9)
+            lbMain.Items.Add(Val(txtNumber.Text) & " x " & txtNumber2 & " = " & txtNumber2 * Val(txtNumber.Text))
+            txtNumber2 += 1
+
+        Loop
+
+    End Sub
+
+    Private Sub btnForNext_Click(sender As Object, e As EventArgs) Handles btnForNext.Click
+        ' Displays a multiplication table.
+        Dim i As Integer
+        lbMain.Items.Clear()
+        lbMain.Refresh()
+
+        For i = 1 To 9
+            lbMain.Items.Add(Val(txtNumber.Text) & " x " & i & " = " & i * Val(txtNumber.Text))
+
+        Next
+
+    End Sub
+
+    Private Sub txtNumber_Enter(sender As Object, e As EventArgs) Handles txtNumber.Enter
+        txtNumber.SelectAll()
+    End Sub
+
+    Private Sub txtNumber_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtNumber.KeyPress
+        ' Allow only numbers and the Backspace key.
+
+        If (e.KeyChar < "0" OrElse e.KeyChar > "9") AndAlso e.KeyChar <> ControlChars.Back Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub txtNumber_TextChanged(sender As Object, e As EventArgs) Handles txtNumber.TextChanged
+        lbMain.Text = String.Empty
+        lbMain.Items.Clear()
+        lbMain.Refresh()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblNameTitle.Click
+
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
